0dd2035b3cf002a90c5dd59a8c10603de09a6f50
Priyank Thakur <priyankthakur3@live.com>
2024-02-04 23:29:14 -0500

    refractor

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -2,7 +2,8 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
-export default function PrivateRoute({ children, reddirectTo }) {
+export default function PrivateRoute({ children, reddirectTo, reverse }) {
   const { currentUser } = useAuth();
+  if (reverse) return currentUser ? <Navigate to={reddirectTo} /> : children;
   return currentUser ? children : <Navigate to={reddirectTo} />;
 }
